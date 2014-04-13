f4a7fbef31f1acc0f85ac42318a91079d62bec13
Bassem Youssef <bassem@outlook.com>
2014-04-12 22:49:03 -0700

    animal soundboard silliness

---
+++ animals.erl
@@ -0,0 +1,38 @@
+-module(animals).
+-compile(export_all). % replace with export later
+
+start() ->
+    spawn(?MODULE,restarter,[]).
+
+pet_sounds() ->
+    receive
+        {From, Ref, {"Miaw"}} ->
+            From ! {Ref, cat};
+        {From, Ref, {"Ruff"}} ->
+            From ! {Ref, dog};
+        {From, Ref, {_}} ->
+            From ! {Ref, what_says_you}
+    end,
+    pet_sounds().
+
+pet_analyze(AnimalSound) ->
+    Ref = make_ref(),
+    pet_soundboard ! {self(), Ref, {AnimalSound}},
+    receive
+        {Ref, WhoDoneIt} -> WhoDoneIt
+    after 2000 ->
+        timeout
+    end.
+
+restarter() ->
+    process_flag(trap_exit,true),
+    Pid = spawn_link(?MODULE, pet_sounds, []),
+    register(pet_soundboard,Pid),
+    receive
+        {'EXIT', Pid, normal} -> % not a crash
+            ok;
+        {'EXIT', Pid, shutdown} -> % manual shutdown, not a crash
+            ok;
+        {'EXIT', Pid, _} ->
+            restarter()
+    end.
