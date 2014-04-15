04a75a390863b181b9c8e9a06f35da1afe36995c
Bassem Youssef <bassem@outlook.com>
2014-04-15 08:36:54 -0700

    Some cleanup

--- session_gen_server.erl
+++ session_gen_server.erl
@@ -1,7 +1,7 @@
 -module(session_gen_server).
 -behavior(gen_server).
--export([start/0, stop/0, create_session/2, get_session/1,
-         destroy_session/1, terminate_server/0]).
+-export([start/0, create_session/2, get_session/1,
+         destroy_session/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 -compile(export_all). % replace with export later
@@ -9,7 +9,6 @@
 
 %% Client APIs
 start() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-stop()  -> gen_server:call(?MODULE, stop).
 
 %% Asynchronous call
 create_session(Identifier, Expr) ->
@@ -22,16 +21,10 @@ get_session(Identifier) ->
 destroy_session(Identifier) ->
     gen_server:call(?MODULE, {destroy, Identifier}).
 
-%% Synchronous call
-terminate_server() ->
-    gen_server:call(?MODULE, terminate).
-
 %% Server APIs
 
 init([]) -> {ok, maps:new()}.
 
-%% Check if we have room for another session, or user has an existing
-%% session already
 handle_cast({create, Identifier, Expr}, Sessions) ->
     %% if session already exists, ignore, if key is in Sessions
     case maps:is_key(Identifier, Sessions) of
@@ -52,18 +45,13 @@ handle_call({destroy, Identifier}, _From, Sessions) ->
 handle_call({get, Identifier}, _From, Sessions) ->
     Reply = case maps:is_key(Identifier, Sessions) of
                 true -> {ok, maps:get(Identifier,Sessions)};
-                false -> io:format("Identifier not found in Sessions ~p~n",
+                false -> io:format("Identifier ~p not found in Sessions ~n",
                                   [Identifier]),
                         {not_found, Identifier}
             end,
-    {reply, Reply, Sessions};
+    {reply, Reply, Sessions}.
 
-handle_call({terminate}, _From, Sessions) ->
-    {stop, normal, ok, Sessions}.
-
-terminate(normal, Sessions) ->
-    [io:format("~p Id was trashed.~n",[K]) || K <- maps:keys(Sessions)],
-    ok.
+terminate(_Reason, _Sessions) -> ok.
 
 handle_info(Msg, Sessions) ->
     io:format("Unexpected messageL ~p~n",[Msg]),
